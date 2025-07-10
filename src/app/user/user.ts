5b9e42601eedc56eff987b9170322371b4fde6f2
Stephanie <stephaniec328122716@gmail.com>
2025-07-09 22:37:28 -0700

    dynamic output for user name

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
 @Component({
     selector: 'app-user',
     standalone: true,
     templateUrl: './user.html',
     styleUrl: './user.css',
 })
-export class UserComponent {}
+export class UserComponent {
+    selectedUser = DUMMY_USERS[randomIndex];
+}
