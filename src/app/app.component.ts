b29d65e4a8260f1837b161cc84c48db7ba598f9d
Stephanie <stephaniec328122716@gmail.com>
2025-07-10 16:02:01 -0700

    configurable name display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,25 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header';
 import { UserComponent } from './user/user';
 import { DUMMY_USERS } from './dummy-users';
+import { WishlistComponent } from './wishlist/wishlist';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, WishlistComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserId = 'u1';
+
+  get selectedUser(){
+    return this.users.find(user => user.id === this.selectedUserId)!;
+  }
 
   onSelectUser(id: string){
-    console.log('Selected user with id: ' + id);
+    this.selectedUserId = id;
 
   }
 }
--- src/app/wishlist/wishlist.ts
+++ src/app/wishlist/wishlist.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'app-wishlist',
     standalone: true, 
     templateUrl: './wishlist.html',
     styleUrl: './wishlist.css',
+    imports: []
 })
 
-export class WishlistComponent {}
+export class WishlistComponent {
+    @Input({required: true}) name!: string;
+}
