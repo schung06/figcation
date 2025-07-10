66897085f589acc648c157ae7805ec7fcc4a6f40
Stephanie <stephaniec328122716@gmail.com>
2025-07-09 22:26:09 -0700

    add user profile component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
-import { HeaderComponent } from '../components/header/header';
+import { HeaderComponent } from './header/header';
+import { UserComponent } from './user/user';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/user/user.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-user',
+    standalone: true,
+    templateUrl: './user.html',
+    styleUrl: './user.css',
+})
+export class UserComponent {}
