be93a414d7d52e9d330ac7e8077d1ee4b50d412a
Stephanie <stephaniec328122716@gmail.com>
2025-07-11 21:24:02 -0700

    output wish

--- src/app/wishlist/wishlist.ts
+++ src/app/wishlist/wishlist.ts
@@ -10,5 +10,38 @@ import { WishComponent } from './wish/wish';
 })
 
 export class WishlistComponent {
-    @Input() name?: string;
+  @Input({required: true}) name!: string;
+  @Input({required: true}) userId!: string;
+
+    wishes = [
+        {
+    id: 't1',
+    userId: 'u1',
+    title: 'Tokyo Skytree',
+    summary:
+      'Visit the tallest structure in Japan and enjoy panoramic views of the city.',
+    dueDate: '2025-12-31',
+      },
+  
+  {
+    id: 't2',
+    userId: 'u2',
+    title: 'Studio Ghibli Museum',
+    summary: 'Explore the magical world of Studio Ghibli with exhibits and short films.',
+    dueDate: '2024-05-31',
+  },
+  {
+    id: 't3',
+    userId: 'u3',
+    title: 'Teamlab Borderless',
+    summary:
+      'Experience a digital art museum with immersive installations and interactive exhibits.',
+    dueDate: '2024-06-15',
+  },
+
+    ];
+  
+  get selectedUserWishes() {
+    return this.wishes.filter((wish) =>  wish.userId === this.userId);
+  }
 }
