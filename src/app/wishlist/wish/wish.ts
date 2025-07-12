3425d36ae1ce3a4540c98d387bd9cda70ec46d7e
Stephanie <stephaniec328122716@gmail.com>
2025-07-11 20:37:20 -0700

    wishlist styling

---
+++ src/app/wishlist/wish/wish.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'app-wish',
+    standalone: true, 
+    templateUrl: './wish.html',
+    styleUrl: './wish.css',
+    imports: []
+})
+
+export class WishComponent {
+    
+}
--- src/app/wishlist/wishlist.ts
+++ src/app/wishlist/wishlist.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { WishComponent } from './wish/wish';
 
 @Component({
     selector: 'app-wishlist',
     standalone: true, 
     templateUrl: './wishlist.html',
     styleUrl: './wishlist.css',
-    imports: []
+    imports: [WishComponent]
 })
 
 export class WishlistComponent {
